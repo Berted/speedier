31c25fbd0fe23d9c1ab133d704f88ad2e2cdcced
Berted <46164341+Berted@users.noreply.github.com>
2022-01-07 17:35:56 +0800

    Audio playback spped optimization

--- background.js
+++ background.js
@@ -1,19 +1,62 @@
 const docbody = document.getElementsByTagName("body")[0]
 const config = {childList: true, subtree: true };
 
-console.log("BACKGROUND.JS RUNNING");
-console.log(docbody)
+let LOUD_SPEED = 1, QUIET_SPEED = 4, THRESHOLD = 0.01
+
+let ctx, 
+processor,
+source,
+video,
+DEBUG_COUNTER = 0,
+start = performance.now();
 
 const optimizeVideo = function(mutationsList, observer)
 {
     let videos = document.getElementsByTagName("video");
 
-    const LOUD_SPEED = 1.5
-    const QUIET_SPEED = 5
-
     if (videos.length) {
-        console.log("A video is found, increasing video playback rate: ");
-        videos[0].playbackRate = 3;
+        video = videos[0];
+        ctx = new AudioContext();
+        // 2048 sample buffer, 1 channel in, 1 channel out 
+        processor = ctx.createScriptProcessor(2048, 1, 1);
+            
+        video.addEventListener('canplaythrough', function() {
+            source = ctx.createMediaElementSource(video)
+            source.connect(processor)
+            source.connect(ctx.destination)
+            processor.connect(ctx.destination)
+        }, false);
+
+        video.addEventListener('play', () => {
+            ctx.resume();
+        });
+        
+        // loop through PCM data and calculate average
+        // volume for a given 2048 sample buffer
+        processor.onaudioprocess = function(evt){
+            var input = evt.inputBuffer.getChannelData(0)
+            , len = input.length   
+            , total = i = 0
+            , max = 0
+            , rms
+            
+            while ( i < len ) 
+            {
+                let nex = Math.abs(input[i++])
+                if (nex > max) {max = nex}
+                if(max > THRESHOLD){
+                    break;
+                }
+            }
+            if (max < THRESHOLD) {video.playbackRate = QUIET_SPEED;}
+            else {video.playbackRate = LOUD_SPEED;}
+            DEBUG_COUNTER++;
+            console.log("wurk: " + (performance.now() - start) + "ms, " + DEBUG_COUNTER + " " + video.playbackRate + " " + max);
+            start = performance.now();
+        };
+    
+        console.log("A video is found!");
+        observer.disconnect();
     }
     else {
         console.log("No videos found!");
@@ -22,4 +65,6 @@ const optimizeVideo = function(mutationsList, observer)
 
 const observer = new MutationObserver(optimizeVideo);
 
+console.log('loaded')
+optimizeVideo([], observer)
 observer.observe(docbody, config);
