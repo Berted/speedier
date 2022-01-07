5bf6484a1c966aa32085609039ccd2302e3bb778
Berted <46164341+Berted@users.noreply.github.com>
2022-01-07 14:23:03 +0800

    MutationObserver added for better video detection

--- background.js
+++ background.js
@@ -1,9 +1,25 @@
-let videos = document.getElementsByTagName("video");
+const docbody = document.getElementsByTagName("body")[0]
+const config = {childList: true, subtree: true };
 
-if (videos.length) {
-    console.log("A video is found, increasing video playback rate: ");
-    videos[0].playbackRate = 3;
+console.log("BACKGROUND.JS RUNNING");
+console.log(docbody)
+
+const optimizeVideo = function(mutationsList, observer)
+{
+    let videos = document.getElementsByTagName("video");
+
+    const LOUD_SPEED = 1.5
+    const QUIET_SPEED = 5
+
+    if (videos.length) {
+        console.log("A video is found, increasing video playback rate: ");
+        videos[0].playbackRate = 3;
+    }
+    else {
+        console.log("No videos found!");
+    }
 }
-else {
-    console.log("No videos found!");
-}
+
+const observer = new MutationObserver(optimizeVideo);
+
+observer.observe(docbody, config);
